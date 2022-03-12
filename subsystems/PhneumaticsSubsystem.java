e2cd5a32a920044b0fe1a3ab22fb5b7a9c94ccf5
IronKings-5402 <61085777+IronKings-5402@users.noreply.github.com>
2022-03-12 10:09:10 -0500

    Add files via upload

--- subsystems/PhneumaticsSubsystem.java
+++ subsystems/PhneumaticsSubsystem.java
@@ -1,13 +1,13 @@
 package frc.robot.subsystems;
 
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class PhneumaticsSubsystem extends SubsystemBase{
 
-    private final Compressor comp = new Compressor();
-    private final DoubleSolenoid solenoid= new DoubleSolenoid(0,1); //not sure if it is double or single
+    private final DoubleSolenoid solenoid= new DoubleSolenoid(PneumaticsModuleType.REVPH,0,1);
  
     public void release()
     {
@@ -15,6 +15,6 @@ public class PhneumaticsSubsystem extends SubsystemBase{
     }
     public void pull()
     {
-        solenoid.set(DoubleSolenoid.Value.kResverse);
+        solenoid.set(DoubleSolenoid.Value.kReverse);
     }
 }
